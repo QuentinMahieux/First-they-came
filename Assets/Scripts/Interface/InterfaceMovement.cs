3dd971fe55eb73c984aa245689650786e9eb87d4
Quentin MAHIEUX <q.mahieux@rubika-edu.com>
2026-04-05 21:23:44 -0400

    Add InterfaceMovement

---
+++ Assets/Scripts/Interface/InterfaceMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class InterfaceMovement : MonoBehaviour, IDragHandler
+{
+    public Canvas canvas;
+    
+    private RectTransform rectTransform;
+    
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    
+}
